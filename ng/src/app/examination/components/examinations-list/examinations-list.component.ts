68f1987b1b5ec5cb0253ee5b94e2aa79cae6639a
Poulad <poulad.pld@gmail.com>
2018-04-23 21:55:29 -0400

    Show previous examinations

--- ng/src/app/examination/components/examinations-list/examinations-list.component.ts
+++ ng/src/app/examination/components/examinations-list/examinations-list.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Examination } from '../../models/examination';
 
 @Component({
     selector: 'app-examinations-list',
     templateUrl: './examinations-list.component.html'
 })
 export class ExaminationsListComponent implements OnInit {
+    @Input() examinations: Examination[];
 
     constructor() {
     }
 
     ngOnInit() {
     }
-
 }
